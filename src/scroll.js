3ba8e09255b44915637217e8a37761c86e9b55b0
narek.gabrielyan <narek.gabrielyan@digitain.com>
2022-05-29 13:12:50 +0400

    doc: JSDoc added

--- src/scroll.js
+++ src/scroll.js
@@ -1,4 +1,19 @@
+/**
+ *  - - - OverlayScroll class - - -
+ * @class
+ * Includes all functionality which is used to create OverlayScroll component
+ */
 class OverlayScroll {
+    /**
+     * @constructor
+     * Takes the target container scrollable element and number of sections as an argument
+     * @param containerEl
+     * @param sectionCount
+     * @example
+     * const exampleContainer = document.getElementById('example_container_element');
+     * const overlayScroll = new OverlayScroll(exampleContainer, 12);
+     * // creates an overlayScroll component in the element with an id 'example_container_element'
+     */
     constructor(containerEl, sectionCount = 2) {
         this.containerEl = containerEl;
         this.sectionCount = sectionCount;
@@ -7,12 +22,28 @@ class OverlayScroll {
             this.init();
         }
     }
-
+    /**
+     * @method onNextSection
+     * Initializes an overlayScroll class and appends the overlayScroll component into given container element
+     */
     init() {
         this.createUI();
     }
-
-
+    /**
+     * @methods onNextSection and onPrevSection
+     * Are used to scroll the overlayScroll bar to the next or the previous sections
+     * @example
+     * window.addEventListener('keydown', (e) => {
+     *   if(e.keyCode === 39) {
+     *     overlayScroll.onNextSection();
+     *   } else if(e.keyCode === 37) {
+     *     overlayScroll.onPrevSection();
+     *   }
+     * });
+     * // listening for keydown event and changing to the next
+     * // or the previous sections when the right
+     * // or the left arrows are being pressed accordingly
+     */
     onNextSection() {
         const {currentSection, sectionCount} = this;
         if (currentSection < sectionCount - 1) {
@@ -20,7 +51,6 @@ class OverlayScroll {
             this.onChangeSection();
         }
     }
-
     onPrevSection() {
         const {currentSection} = this;
         if (currentSection > 0) {
@@ -29,6 +59,10 @@ class OverlayScroll {
         }
     }
 
+    /**
+     * @method onChangeSection
+     * Changes the scrollbar thus making the scroll changing animation
+     */
     onChangeSection() {
         const {track, decimals, units, currentSection} = this;
         track.style.transform = `translateY(${(currentSection) * 100}%)`;
@@ -36,6 +70,10 @@ class OverlayScroll {
         units.style.transform = `translateY(${currentSection * (-100)}%)`;
     }
 
+    /**
+     * @method createUI
+     * Creates html and adds necessary styles
+     */
     createUI() {
         const {containerEl, sectionCount} = this;
 
@@ -66,9 +104,12 @@ class OverlayScroll {
         this.decimals = decimals;
         this.units = units;
     }
-
+    /**
+     * @method destroy
+     * Call this method whenever you want to remove the overlayScroll component
+     */
     destroy() {
-        const { wrapper } = this;
+        const {wrapper} = this;
         wrapper.remove();
     }
 }
